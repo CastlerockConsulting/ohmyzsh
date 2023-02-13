f58a514b71bf234fb816d9b04ab0b3a9d42eb055
Dan Goldberg <dan@castlerockcs.com>
2023-02-13 09:01:42 -0500

    adds the region to the aws profile prompt

--- plugins/aws/aws.plugin.zsh
+++ plugins/aws/aws.plugin.zsh
@@ -159,7 +159,8 @@ compctl -K _aws_profiles asp acp aws_change_access_key
 # AWS prompt
 function aws_prompt_info() {
   [[ -n "$AWS_PROFILE" ]] || return
-  echo "${ZSH_THEME_AWS_PREFIX=<aws:}${AWS_PROFILE:gs/%/%%}${ZSH_THEME_AWS_SUFFIX=>}"
+  #echo "${ZSH_THEME_AWS_PREFIX=<aws:}${AWS_PROFILE:gs/%/%%}${ZSH_THEME_AWS_SUFFIX=>}"
+  echo "${ZSH_THEME_AWS_PREFIX:=<aws:}${AWS_PROFILE}:${AWS_DEFAULT_REGION}${ZSH_THEME_AWS_SUFFIX:=>}"
 }
 
 if [[ "$SHOW_AWS_PROMPT" != false && "$RPROMPT" != *'$(aws_prompt_info)'* ]]; then
